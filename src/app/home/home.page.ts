74505b0854944123fc2b44ef51b6ebbcd457fd18
Denthor99 <darancuga@hotmail.com>
2023-10-25 10:33:09 +0200

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  movies: any;
+
+  constructor(private httpClient:HttpClient) {
+    this.movies = this.httpClient.get();
+  }
+
+}
